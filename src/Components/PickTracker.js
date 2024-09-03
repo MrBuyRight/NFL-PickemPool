81e1db455362e2f8f89b40810501da42540ab69b
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-03 00:55:50 -0700

    Simplify PickTracker component design

--- src/Components/PickTracker.js
+++ src/Components/PickTracker.js
@@ -2,26 +2,24 @@ import React from 'react';
 import './PickTracker.css';
 
 const PickTracker = ({ picks, games }) => {
-  const progress = (Object.keys(picks).length / games.length) * 100;
+  const picksCount = Object.keys(picks).length;
+  const progress = (picksCount / games.length) * 100;
 
   return (
     <div className="pick-tracker">
       <h3>Your Picks</h3>
-      <ul>
-        {games.map((game) => (
-          <li key={game.id} className="pick-item">
-            <span className="game-teams">
-              {game.awayTeam} @ {game.homeTeam}
-            </span>
-            <span className={`pick-result ${picks[game.id] ? 'picked' : ''}`}>
-              {picks[game.id] || 'Not picked'}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="picks-summary">
+        <span className="picks-count">{picksCount}</span>
+        <span className="picks-total">/ {games.length}</span>
+      </div>
       <div className="progress-bar">
         <div className="progress" style={{width: `${progress}%`}}></div>
       </div>
+      <ul className="picks-list">
+        {Object.entries(picks).map(([gameId, team]) => (
+          <li key={gameId} className="pick-item">{team}</li>
+        ))}
+      </ul>
     </div>
   );
 };
