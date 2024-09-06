adda1addeb17b57529f94b7acf5aeba6c62edf5c
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-05 22:15:54 -0700

    Optimized app for mobile UI/UX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Leaderboard from './Components/Leaderboard';
 import './App.css'; // Make sure you have this if you're using App-specific styles
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Helmet>
       <h1>NFL Week 1 Pick'em Pool</h1>
       <Leaderboard />
     </div>
