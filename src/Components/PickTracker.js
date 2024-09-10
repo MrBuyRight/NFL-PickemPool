dcb2716e6ae9e8d6d8b7ae741afcfeaadd303220
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-10 11:36:08 -0700

    Move PickTracker to the right and simplify pick display

--- src/Components/PickTracker.js
+++ src/Components/PickTracker.js
@@ -8,14 +8,11 @@ const PickTracker = ({ selectedPicks, games }) => {
     <div className="pick-tracker">
       <h3>Your Picks</h3>
       <ul>
-        {sortedPicks.map(([gameId, team]) => {
-          const game = games.find(g => g.id === parseInt(gameId));
-          return (
-            <li key={gameId}>
-              {game.awayTeam} @ {game.homeTeam}: <span className="selected-team">{team}</span>
-            </li>
-          );
-        })}
+        {sortedPicks.map(([gameId, team]) => (
+          <li key={gameId}>
+            <span className="selected-team">{team}</span>
+          </li>
+        ))}
       </ul>
       <p>{sortedPicks.length} of {games.length} picks made</p>
     </div>
