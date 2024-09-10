f82b8fef4a0843079f8326455cc9716c018cc207
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-10 10:03:05 -0700

    Update package.json and reinstal dependencies

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+let supabase;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+try {
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+  const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL or Anon Key is missing')
+  }
+
+  supabase = createClient(supabaseUrl, supabaseAnonKey)
+} catch (error) {
+  console.error('Error initializing Supabase client:', error)
+}
+
+export { supabase }
