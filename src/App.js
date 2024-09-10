6565d244d18e89f10cf7f893b46eaea93698383b
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-10 11:27:51 -0700

    Add GameSelectionList component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GameSelectionList from './Components/GameSelectionList';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>NFL Pick'em Pool</h1>
       <p>This is a test render of the App component.</p>
+      <GameSelectionList />
     </div>
   );
 }
--- src/Components/GameSelectionList.js
+++ src/Components/GameSelectionList.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const GameSelectionList = () => {
-	console.log('Rendering GameSelectionList');
 	return (
 		<div className="game-selection-list">
-			<h2>Game Selection List</h2>
-			<p>This is a test render of the GameSelectionList component.</p>
+			<h2>Game Selection</h2>
+			<p>This is where the games will be listed for selection.</p>
 		</div>
 	);
 };
