e80d26e77fcc5b29b7641e1678b502f93393eada
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-13 13:48:50 -0700

    Update App header font to a more modern option

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pickem Pool - Week 2</h1>
+        <h1 className="App-title"> Week 2 Pick 'em Pool</h1>
       </header>
       <main>
         <Leaderboard entries={entriesData} />
