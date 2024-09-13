ac5324a1ba5fe427652ac3accd3e6336d750b2bb
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-13 11:26:57 -0700

    Simplify App and Leaderboard components for troubleshooting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Leaderboard from './Components/Leaderboard';
 
 function App() {
+  console.log('App component is rendering');
   return (
     <div className="App">
+      <h1>NFL Pick'em Pool</h1>
       <Leaderboard />
     </div>
   );
