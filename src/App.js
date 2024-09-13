920d022b689acc59397c8d569cb1211e61c1cf68
Cody Golbad <codyg@Codys-MacBook-Pro.local>
2024-09-13 11:57:37 -0700

    Import entriesData directly in Leaderboard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Leaderboard from './Components/Leaderboard';
-import entriesData from './Components/entriesData';
 
 function App() {
-  console.log('App rendering, entriesData length:', entriesData.length);
   return (
     <div className="App">
       <h1>NFL Pick'em Pool</h1>
-      <Leaderboard entries={entriesData} />
+      <Leaderboard />
     </div>
   );
 }
--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Leaderboard.css';
+import entriesData from './entriesData';  // Import entriesData directly
 
-function Leaderboard({ entries }) {
+function Leaderboard() {  // Remove the { entries } prop
   console.log('Leaderboard component is rendering');
-  console.log('Number of entries:', entries.length);
+  console.log('Number of entries:', entriesData.length);
 
   return (
     <div className="leaderboard" style={{backgroundColor: 'lightgray', padding: '20px'}}>
       <h2>Leaderboard</h2>
       <p>The leaderboard is currently being updated. Check back soon!</p>
+      <p>Number of entries: {entriesData.length}</p>  // Display the number of entries
     </div>
   );
 }
